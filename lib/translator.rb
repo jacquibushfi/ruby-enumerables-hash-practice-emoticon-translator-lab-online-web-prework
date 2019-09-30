cf94c3ec04a1dab642ba06fd9a49a92340fdbefc
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-30 14:24:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,11 @@ def get_japanese_emoticon(file_path = '.lib/emoticons.yml', japanese_emoticon)
 end
   
  
-def get_english_meaning
-  # code goes here
+def get_english_emoticon(file_path = '.lib/emoticons.yml', english_emoticon)
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon)
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
