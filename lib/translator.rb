67eb7b8e62eabedb61f3bd4d3b4c14a6413a2110
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-30 14:20:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,10 +12,10 @@ loadlib.each do |meaning, emoticons|
 
 load_library('./lib/emoticons.yml')
 
-def get_japanese_emoticon(file_path = './lib/emoticons.yml/, english_emoticon')
+def get_japanese_emoticon(file_path = './lib/emoticons.yml/, japanese_emoticon')
   library = load_library(file_path)
-  if library["get_emoticon"].include?(english_emoticon)
-    library["get_emoticon"][english_emoticon]
+  if library["get_emoticon"].include?(japanese_emoticon)
+    library["get_emoticon"][japanese_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
